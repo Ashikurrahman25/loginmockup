2f2f6a74d466adc5d4a8493da8ae8bc313190402
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-12-04 12:05:02 +0600

    new

--- src/VerifyPage.js
+++ src/VerifyPage.js
@@ -103,7 +103,7 @@ const VerifyPage = () => {
           type: "FunctionCall",
           params: {
             methodName: "ft_transfer",
-            args: {receiver_id: contractId, amount: amountToPay, memo: "Entry fee for a match on SpearOnNear Game 1"},
+            args: {receiver_id: contractId, amount: amountToPay.toString(), memo: "Entry fee for a match on SpearOnNear Game 1"},
             gas: 10000000000000,
             deposit: 1
           }
